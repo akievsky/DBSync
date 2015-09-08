9cd49682933ca9864e6439a2f4733051c45538d5
Oleksandr Kyivskyi <okyivskyi@Oleksandrs-MacBook-Pro.local>
2015-09-08 12:12:45 +0300

    Added empty project

---
+++ DBSyncWrapper/DBSync/Session.swift
@@ -0,0 +1,9 @@
+//
+//  Session.swift
+//  DBSyncWrapper
+//
+//  Created by Oleksandr Kyivskyi on 9/7/15.
+//  Copyright (c) 2015 Oleksandr Kyivskyi. All rights reserved.
+//
+
+import Foundation
---
+++ DBSyncWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBSyncWrapper
+//
+//  Created by Oleksandr Kyivskyi on 9/7/15.
+//  Copyright (c) 2015 Oleksandr Kyivskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
